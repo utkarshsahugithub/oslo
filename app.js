b45965f87aaa3f291c6b8ec63832709a401f99c6
Utkarsh Sahu <utkarshsahugithub@gmail.com>
2020-12-09 18:01:33 +0530

    right btn is working

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
     $('.carousel img').not(".active").hide();
+    // $(".slide").each(function(index, element) {
+    //     // $(this).find('img').show();
+    //     setTimeout(() => {  
+    //         console.log(index + ": " + $(this).html());
+    //     }, 5000);
+    // })
+    function foo(time, element) {
+        setTimeout(() => {
+            $(element).addClass(".active");
+            element = $(element).next();
+            console.log(element);
+        }, time);
+        time += 1000;
+        foo(time, element)
+    }
 
+    var array = $(".slide").toArray();
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        var time = 1000;
+        foo(time, element);
+        // console.log(element);
+    }
 })
---
+++ main.js
@@ -0,0 +1,43 @@
+let array = document.querySelectorAll(".slide");
+let index = 0;
+function foo(index) {
+    array[index-1].firstChild.classList.remove("active");
+    array[index-1].firstChild.classList.add("inactive");
+}
+function showBanner(index) {
+    console.log(array[index].firstChild);
+    array[index].firstChild.classList.add("active");
+    array[index].firstChild.classList.remove("inactive");
+    if(index !== 0) {
+        foo(index);
+    } 
+    else{
+        array[array.length-1].firstChild.classList.remove("active");
+        array[array.length-1].firstChild.classList.add("inactive");
+    }
+}
+let rightBtn = document.querySelector('.right');
+rightBtn.addEventListener('click', () => {
+    index++;
+    if(index<array.length) {
+        showBanner(index);
+        
+    } else if (index===array.length) {
+        index=0;
+        showBanner(index);
+        
+    }
+})
+
+let leftBtn = document.querySelector('.left');
+leftBtn.addEventListener('click', () => {
+    if(index!==array.length) {
+        showBanner(index);
+        index--;
+    } else if (index===0) {
+        console.log('hello');
+        foo(index);
+        index=3;
+        showBanner(index);
+    }
+})
