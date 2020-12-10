f738d7c62b59ea613e5bc083601a772879c5c986
Utkarsh Sahu <utkarshsahugithub@gmail.com>
2020-12-10 16:15:43 +0530

    proper working of button and pagination is working

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
     let leftBtn = $('.left');
     let circle = $(".circle");
     let index = 0;
+    var timingVariable;
     
     rightBtn.on('click', () => {
+        clearInterval(timingVariable);
+        timingVariable = null;
         index++;
         if(index<array.length) {
             $(".slide").children().removeClass("active").addClass("inactive");
@@ -15,8 +18,11 @@ $(document).ready(function() {
             $(".slide").children().removeClass("active").addClass("inactive");
             $("#slide"+[index+1]).children().addClass("active").removeClass("inactive");
         }
+        if(timingVariable == null) timingFunction();
     })
     leftBtn.on('click', () => {
+        clearInterval(timingVariable);
+        timingVariable = null;
         index--;
         if(index<0){
             // console.log(index);
@@ -29,25 +35,41 @@ $(document).ready(function() {
             $(".slide").children().removeClass("active").addClass("inactive");
             $("#slide"+[index+1]).children().addClass("active").removeClass("inactive");
         }
+        if(timingVariable == null) timingFunction();
     })
+    function changeDotColor(i) {
+        for(let j=0; j<circle.length; j++) {
+            $(circle[j]).removeClass('circleColor');
+        }
+        $(circle[i]).addClass('circleColor');
+    }
+    
     for(let i=0; i<circle.length; i++) {
         $(circle[i]).on('click', () => {
             // console.log(circle[i]);
             // console.log($("#slide"+[i+1]));
+            clearInterval(timingVariable);
+            timingVariable = null;
             $(".slide").children().removeClass("active").addClass("inactive");
             $("#slide"+[i+1]).children().addClass("active").removeClass("inactive");
+            if(timingVariable == null) timingFunction();
+            changeDotColor(i);
         })
     }
-
-    setInterval(() => {
-        if(index<2) {
-            $(".slide").children().removeClass("active").addClass("inactive");
-            $("#slide"+[index+1]).children().addClass("active").removeClass("inactive");
-            index++;
-        } else {
-            $(".slide").children().removeClass("active").addClass("inactive");
-            $("#slide"+[index+1]).children().addClass("active").removeClass("inactive");
-            index=0;
-        }
-    }, 2000);
+    function timingFunction() {
+        timingVariable = setInterval(() => {
+            if(index<2) {
+                $(".slide").children().removeClass("active").addClass("inactive");
+                $("#slide"+[index+1]).children().addClass("active").removeClass("inactive");
+                changeDotColor(index);
+                index++;
+            } else {
+                $(".slide").children().removeClass("active").addClass("inactive");
+                $("#slide"+[index+1]).children().addClass("active").removeClass("inactive");
+                changeDotColor(index);
+                index=0;
+            }
+        }, 2000);
+    }
+    timingFunction();
 })
